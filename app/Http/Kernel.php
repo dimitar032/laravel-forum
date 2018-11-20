e6783c3af9d41205c964cd843b7019464c503d16
Dimitar Dimitrov <ddimitrov@intracol.com>
2018-11-20 11:17:51 +0000

    push to demo1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,6 +11,7 @@ class Kernel extends HttpKernel
      *
      * These middleware are run during every request to your application.
      *
+     * add something 
      * @var array
      */
     protected $middleware = [
